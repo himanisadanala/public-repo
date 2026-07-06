427ef55d9c741fcb92d3faf468c9b615fc2459ff
himanisadanala <himanisadanala@gmail.com>
2026-07-06 15:00:47 +0530

    new commit

---
+++ hima.py
@@ -0,0 +1,13 @@
+username= "himani"
+followers= 1000
+verified= True
+rating= 4.4
+
+print(username)
+print(followers)
+print(verified)
+print(rating)
+username=(str(input("enter your name ")))
+followers=(int(input("enter the value ")))
+vreified=(bool(input("enter the boolean value ")))
+rating=(float(input("enter the float value ")))
